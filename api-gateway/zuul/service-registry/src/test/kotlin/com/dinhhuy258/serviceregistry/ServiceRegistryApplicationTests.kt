64065045018fbd59895018a86e154ffef5651425
Huy Duong <huy.duongdinh@gmail.com>
2022-06-19 00:20:20 +0700

    Service registry

---
+++ api-gateway/zuul/service-registry/src/test/kotlin/com/dinhhuy258/serviceregistry/ServiceRegistryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dinhhuy258.serviceregistry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
