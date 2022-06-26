04f64248603d7687b6955a42a6dd8974c36c854d
Huy Duong <huy.duongdinh@gmail.com>
2022-06-26 23:54:27 +0700

    Zuul proxy

---
+++ api-gateway/zuul/zuul-proxy/src/test/kotlin/com/dinhhuy258/zuulproxy/ZuulProxyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dinhhuy258.zuulproxy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZuulProxyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
